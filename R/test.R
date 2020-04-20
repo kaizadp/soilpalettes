964c9483f7e508a7866ff8d7919b318c17cf7257
Kaizad <kaizad.patel@pnnl.gov>
2020-04-20 10:48:00 -0700

    Update test.R

--- R/test.R
+++ R/test.R
@@ -1,4 +1,4 @@
-source("soilcolors.R")
+source("soilpalettes.R")
 library(ggplot2)
 
 pal=soil_palette("alaquod2", 5,type = "discrete")
@@ -19,11 +19,3 @@ soil_palette(name="paleustalf",n=7,type = "discrete")
 soil_palette(name="natrudoll",n=7,type = "discrete")
 soil_palette(name="alaquod2",n=7,type = "continuous")
 
-
-
-
-rect(1,1,2,2, col = "red")
-rect(100, 300, 125, 350, col = "red") # transparent
-plot(c(100, 200), c(100, 900), type= "n", xlab = "", ylab = "")
-
-print.palette(durorthod)
